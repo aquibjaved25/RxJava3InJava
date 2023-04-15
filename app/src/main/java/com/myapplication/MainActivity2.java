044cdb7533b94c8d35574c2e6143f9878aab8086
aquibjaved25 <aquib.javed25@gmail.com>
2023-04-15 12:13:14 +0530

    buffer added

---
+++ app/src/main/java/com/myapplication/MainActivity2.java
@@ -0,0 +1,58 @@
+package com.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private static final String TAG = "MyApp";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Observable<Integer> myObservable = Observable.range(1,20);
+
+        myObservable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .buffer(4)
+                .subscribe(new Observer<List<Integer>>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                    }
+
+                    @Override
+                    public void onNext(@NonNull List<Integer> integers) {
+                        Log.i(TAG,"onNext");
+
+                        for(Integer i :integers) {
+                            Log.i(TAG,"int value is "+i);
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+
+    }
+}
