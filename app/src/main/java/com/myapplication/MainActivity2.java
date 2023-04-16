8b3f0992bf5433a35cb8a3d0cbebba58a709885a
aquibjaved25 <aquib.javed25@gmail.com>
2023-04-16 10:30:51 +0530

    filter

--- app/src/main/java/com/myapplication/MainActivity2.java
+++ app/src/main/java/com/myapplication/MainActivity2.java
@@ -12,6 +12,7 @@ import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Predicate;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -27,19 +28,22 @@ public class MainActivity2 extends AppCompatActivity {
 
         myObservable.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .buffer(4)
-                .subscribe(new Observer<List<Integer>>() {
+                .filter(integer -> integer%3 == 0)
+                /*.filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer integer) throws Throwable {
+                        return integer%3 == 0;
+                    }
+                })*/
+                .subscribe(new Observer<Integer>() {
                     @Override
                     public void onSubscribe(@NonNull Disposable d) {
+
                     }
 
                     @Override
-                    public void onNext(@NonNull List<Integer> integers) {
-                        Log.i(TAG,"onNext");
-
-                        for(Integer i :integers) {
-                            Log.i(TAG,"int value is "+i);
-                        }
+                    public void onNext(@NonNull Integer integer) {
+                        Log.i(TAG,"onNext Invoked "+integer);
                     }
 
                     @Override
